d29fcfe58411d8f5cd2c92977c129d3bb1fdc59c
Rivaldo Roberto <rrs.sistema@gmail.com>
2022-08-14 23:43:11 -0300

    Usando o NetworkGiffyDialog

--- lib/screens/transferencia/formilario_transferencia.dart
+++ lib/screens/transferencia/formilario_transferencia.dart
@@ -1,3 +1,4 @@
+import 'package:giffy_dialog/giffy_dialog.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
 
@@ -15,9 +16,9 @@ const _dicaCampoNumeroConta = '0000';
 const _textoBotaoConfirmar = 'Confirmar';
 
 class FormularioTransferencia extends StatelessWidget {
-
-final TextEditingController _controladorCampoNumeroConta = TextEditingController();
-final TextEditingController _controladorCampoValor = TextEditingController();
+  final TextEditingController _controladorCampoNumeroConta =
+      TextEditingController();
+  final TextEditingController _controladorCampoValor = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -76,23 +77,62 @@ final TextEditingController _controladorCampoValor = TextEditingController();
     final int numeroConta = int.tryParse(_controladorCampoNumeroConta.text);
     final double valor = double.tryParse(_controladorCampoValor.text);
 
-    if(_validaTransferencia(context, numeroConta, valor)) {
+    if (_validaTransferencia(context, numeroConta, valor)) {
       final novaTransferencia = Transferencia(valor, numeroConta);
       _atualizaEstado(context, novaTransferencia, valor);
-      Navigator.pop(context);      
+      Navigator.pop(context);
+    } else {
+      _showFailureMessage(context, message: 'Parece que você não tem saldos.');
     }
   }
 
   _validaTransferencia(BuildContext context, int numeroConta, double valor) {
     final _camposValidos = numeroConta != null && valor != null;
-    final _saldoSuficiente = valor <= Provider.of<Saldo>(context, listen: false,).valor;
+    final _saldoSuficiente = valor <=
+        Provider.of<Saldo>(
+          context,
+          listen: false,
+        ).valor;
     return _camposValidos && _saldoSuficiente;
   }
 
-  _atualizaEstado(BuildContext context, Transferencia novaTransferencia, double valor) {
-    Provider.of<Transferencias>(context, listen: false).adiciona(novaTransferencia);
+  _atualizaEstado(
+      BuildContext context, Transferencia novaTransferencia, double valor) {
+    Provider.of<Transferencias>(context, listen: false)
+        .adiciona(novaTransferencia);
     Provider.of<Saldo>(context, listen: false).subtrai(valor);
   }
 
+  void _showFailureMessage(BuildContext context,
+      {String message = 'Unknown error'}) {
+    showDialog(
+        context: context,
+        builder: (_) => NetworkGiffyDialog(
+              key: Key('_networkGiffyDialog'),
+              image: Image.network(
+                'https://c.tenor.com/e_6G20pCUj0AAAAj/hereisrae-raexsamlo.gif',
+                fit: BoxFit.cover,
+              ),
+              entryAnimation: EntryAnimation.TOP,
+              title: Text(
+                'OPS!',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 26.0, fontWeight: FontWeight.w600),
+              ),
+              description: Text(
+                message,
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w800),
+              ),
+              onlyCancelButton: true,
+              buttonCancelColor: Colors.blue.shade300,
+              buttonCancelText: Text(
+                'Fechar',
+                style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w800),
+              ),
+              onCancelButtonPressed: () {
+                Navigator.of(context).pop();
+              },
+            ));
+  }
 }
-
